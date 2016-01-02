51ea1c0c7496ca4300ef1ba4eb82cda25e0378b5
bharathkkb <bharathkkb@pixelblenders.com>
2016-01-02 14:49:04 -0800

    Removed super object, fixed csv parsing
    Now stores each obj directly in the Collections.
    The earlier cvs to array parsing function skipped the last line in each
    file. Fixed that with new function.

--- calt.js
+++ calt.js
@@ -9,27 +9,52 @@ fare_attributes= new Meteor.Collection("fare_attributes");
 fare_rules= new Meteor.Collection("fare_rules");
 shapes= new Meteor.Collection("shapes");
 frequencies= new Meteor.Collection("frequencies");
+temp=new Meteor.Collection("temp");
 if (Meteor.isClient) {
   // counter starts at 0
    
 
   Template.datab.helpers({
     'printdb':function(){
-    var x= stops.find().fetch();
-    console.log(x[0][0]);
+    var x= stop_times.find().fetch();
+    console.log(get_json_size(x));
+    for(var i=0;i<(get_json_size(x));i++) {// subtracting 1 because there are 2 extra objects for Collections, hence no need for the last two
+    	// if(get_stop_name(stop_times.find().fetch()[i].stop_id).stop_name){
+    		
+    	console.log(i+" "+"Stop id is : "+stop_times.find().fetch()[i].stop_id+ " Stop Name is "+get_stop_name(stop_times.find().fetch()[i].stop_id).stop_name);
+    // }
+    	
+    	// console.log(i);
+    }
+    // console.log(get_stop_name("NANAA"));
     
+    // console.log(sup());
     /*console.log(x[0][0]["agency_url"]);*/
     /*using this kind of return inroder to make sure data is available 
     and no error is thrown*/
-    return x[0] && x[0][0];
+    return x[0] ;
   
 
-  }
+  },
   });
 
   
 }
-
+get_stop_name= function(stop_id){
+	return stops.findOne(
+{"stop_id":stop_id}
+);
+}
+get_json_size = function(obj) {
+    var size = 0, key;
+    
+    for (key in obj) {
+    	
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    
+    return size;
+};
 if (Meteor.isServer) {
   Meteor.startup(function () {
     var path = Npm.require('path');
@@ -44,12 +69,20 @@ base = base.split('.meteor')[0];
   for(i=0;i<files.length;i++){
   	
   	var json=CSV2JSON(zip.files[files[i]].asText());
+// console.log(json);
   	var jsonobj = JSON.parse(json);
   	 var file_name=files[i].substring(0,(files[i].length-4));
   	 console.log("found "+files[i]);
   	 if(eval(file_name).find().count() === 0){
+
+  	 	temp.insert(jsonobj);
 	    console.log("inserted "+files[i]);
-	    eval(file_name).insert(jsonobj);
+	    var x= temp.find().fetch();
+	    for(var j=0;j<(get_json_size(x[0]))-1;j++) {
+	    	
+	    eval(file_name).insert(temp.find().fetch()[0][j]);
+	}
+	temp.remove({});
 	 		}
 
   	//  switch(files[i]){
@@ -70,15 +103,6 @@ base = base.split('.meteor')[0];
   	//  }
   }
 
- 
- 
-
-
-
-if(agency.find().count() === 0){
-    
-    agency.insert(jsonobj);
- }
     /*for(ag in ags){
       console.log(ags[ah]);*/
     /*}*/
@@ -92,6 +116,7 @@ if(agency.find().count() === 0){
   
 }
 
+
 file_list = function(obj) {
     var size = 0, key;
     var files_array=[];
@@ -103,83 +128,127 @@ file_list = function(obj) {
     
     return files_array;
 };
+
 get_json = function(filename){
 	  var json=CSV2JSON(zip.files[filename].asText());
 var jsonobj = JSON.parse(json);
 }
 
-//ben nadal csv
-function CSVToArray(strData, strDelimiter) {
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
-    strDelimiter = (strDelimiter || ",");
-    // Create a regular expression to parse the CSV values.
-    var objPattern = new RegExp((
-    // Delimiters.
-    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-    // Quoted fields.
-    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-    // Standard fields.
-    "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
-    var arrData = [[]];
-    // Create an array to hold our individual pattern
-    // matching groups.
-    var arrMatches = null;
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
-    while (arrMatches = objPattern.exec(strData)) {
-        // Get the delimiter that was found.
-        var strMatchedDelimiter = arrMatches[1];
-        // Check to see if the given delimiter has a length
-        // (is not the start of string) and if it matches
-        // field delimiter. If id does not, then we know
-        // that this delimiter is a row delimiter.
-        if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
-            // Since we have reached a new row of data,
-            // add an empty row to our data array.
-            arrData.push([]);
-        }
-        // Now that we have our delimiter out of the way,
-        // let's check to see which kind of value we
-        // captured (quoted or unquoted).
-        if (arrMatches[2]) {
-            // We found a quoted value. When we capture
-            // this value, unescape any double quotes.
-            var strMatchedValue = arrMatches[2].replace(
-            new RegExp("\"\"", "g"), "\"");
-        } else {
-            // We found a non-quoted value.
-            var strMatchedValue = arrMatches[3];
-        }
-        // Now that we have our value string, let's add
-        // it to the data array.
-        arrData[arrData.length - 1].push(strMatchedValue);
+//Jos de Jong
+function csv2array(data, delimeter) {
+  // Retrieve the delimeter
+  if (delimeter == undefined) 
+    delimeter = ',';
+  if (delimeter && delimeter.length > 1)
+    delimeter = ',';
+
+  // initialize variables
+  var newline = '\n';
+  var eof = '';
+  var i = 0;
+  var c = data.charAt(i);
+  var row = 0;
+  var col = 0;
+  var array = new Array();
+
+  while (c != eof) {
+    // skip whitespaces
+    while (c == ' ' || c == '\t' || c == '\r') {
+      c = data.charAt(++i); // read next char
     }
-    // Return the parsed data.
-    arrData.pop();
     
-    return (arrData);
+    // get value
+    var value = "";
+    if (c == '\"') {
+      // value enclosed by double-quotes
+      c = data.charAt(++i);
+      
+      do {
+        if (c != '\"') {
+          // read a regular character and go to the next character
+          value += c;
+          c = data.charAt(++i);
+        }
+        
+        if (c == '\"') {
+          // check for escaped double-quote
+          var cnext = data.charAt(i+1);
+          if (cnext == '\"') {
+            // this is an escaped double-quote. 
+            // Add a double-quote to the value, and move two characters ahead.
+            value += '\"';
+            i += 2;
+            c = data.charAt(i);
+          }
+        }
+      }
+      while (c != eof && c != '\"');
+      
+      if (c == eof) {
+        
+      }
+
+      c = data.charAt(++i);
+    }
+    else {
+      // value without quotes
+      while (c != eof && c != delimeter && c!= newline && c != ' ' && c != '\t' && c != '\r') {
+        value += c;
+        c = data.charAt(++i);
+      }
+    }
+
+    // add the value to the array
+    if (array.length <= row) 
+      array.push(new Array());
+    array[row].push(value);
+    
+    // skip whitespaces
+    while (c == ' ' || c == '\t' || c == '\r') {
+      c = data.charAt(++i);
+    }
+
+    // go to the next row or column
+    if (c == delimeter) {
+      // to the next column
+      col++;
+    }
+    else if (c == newline) {
+      // to the next row
+      col = 0;
+      row++;
+    }
+    else if (c != eof) {
+      // unexpected character
+      
+    }
+    
+    // go to the next character
+    c = data.charAt(++i);
+  }  
+  
+  return array;
 }
 //http://jsfiddle.net/sturtevant/AZFvQ/
 function CSV2JSON(csv) {
 
-    var array = CSVToArray(csv);
-
+    var array = csv2array(csv);
+   
     var objArray = {};
     for (var i = 1; i < array.length; i++) {
         objArray[i - 1] = {};
         for (var k = 0; k < array[0].length && k < array[i].length; k++) {
             var key = array[0][k];
             objArray[i - 1][key] = array[i][k]
+
         }
+
     }
     
     var json = JSON.stringify(objArray);
-// console.log(json);
+ 
     var str = json.replace(/},/g, "},\r\n");
-
+     
     return str;
   }
 UI.registerHelper("arrayify", function(obj){
